4667faf8a86bcd5d99ec2f0cde9ec654a6e13198
Zhan <jabberwork.owl@gmail.com>
2023-04-17 17:04:53 +0600

    test

--- src/Dog.java
+++
@@ -1,9 +0,0 @@
-public class Dog extends Animal{
-    Dog(String color, int numberOfPaws, boolean hasFur) {
-        super("brown", 4, true);
-    }
-
-    public Dog() {
-        super();
-    }
-}
--- src/TriangleValidator.java
+++
@@ -1,9 +0,0 @@
-public class TriangleValidator {
-    public void validate(double firstSide, double secondSide, double thirdSide) {
-        if (firstSide + secondSide > thirdSide && firstSide + thirdSide > secondSide && secondSide + thirdSide > firstSide) {
-            System.out.println("This is a valid triangle");
-        } else {
-            System.out.println("It's not a triangle");
-        }
-    }
-}

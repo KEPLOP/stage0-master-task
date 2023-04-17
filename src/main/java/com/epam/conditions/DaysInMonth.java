03ef18ecc182692af6f16aecff70534c88013c73
Zhan <jabberwork.owl@gmail.com>
2023-04-17 18:32:51 +0600

    and check

--- src/main/java/com/epam/conditions/DaysInMonth.java
+++ src/main/java/com/epam/conditions/DaysInMonth.java
@@ -2,31 +2,29 @@ package com.epam.conditions;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
-        // Check if year is valid
-        if (year < 0) {
-            System.out.println("invalid date");
+        if (year < 0 || month < 1 || month > 12) {
+            System.out.println("Invalid date");
             return;
         }
 
-        // Check if month is valid
-        if (month < 1 || month > 12) {
-            System.out.println("invalid date");
-            return;
-        }
+        // Check if leap year
+        boolean isLeap = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
 
-        // Get the number of days in the given month
-        int numDays;
-        if (month == 2) {
-            if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
-                numDays = 29;  // leap year
-            } else {
-                numDays = 28;  // not a leap year
-            }
-        } else if (month == 4 || month == 6 || month == 9 || month == 11) {
-            numDays = 30;
-        } else {
-            numDays = 31;
+        // Calculate number of days in month
+        int days;
+        switch (month) {
+            case 2:
+                days = isLeap ? 29 : 28;
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                days = 30;
+                break;
+            default:
+                days = 31;
+                break;
         }
-
     }
 }
